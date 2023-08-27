8bad0aa30dd63d0deb68fc36458a90a3bc9d4a5e
Pedro Fuentes <31746859+ultraKelevra@users.noreply.github.com>
2023-08-27 15:45:34 -0400

    improve description view in phone

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -12,16 +12,18 @@ section.center.dark-glass
         VerticalLine.left.grow-bottom.delayed.d-075
         VerticalLine.right.grow-top.delayed.d-025
 
-        .column.end.title.whide
-            .column.end.condensed
-                p Max Supply
-                h1 2000
-            .column.end.condensed
-                p Price
-                h1 450K PLS
-            .column.end.condensed
-                p Mints Per Wallet
-                h1 55
+        .column.condensed.whide
+            span.table-title Stats
+            .table.column.whide.stretched.condensed
+                .column.end.condensed
+                    p Max Supply
+                    h1 2000
+                .column.end.condensed
+                    p Price
+                    h1 450K PLS
+                .column.end.condensed
+                    p Mints Per Wallet
+                    h1 55
 
             //- div.column.condensed.start
         p Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, excepturi veniam, perferendis doloribus unde, voluptatum at obcaecati iusto tempore perspiciatis molestiae impedit facilis similique! Velit modi numquam nostrum eveniet dolorem!
@@ -31,6 +33,9 @@ section.center.dark-glass
 </template>
 
 <style lang="sass">
+.table-title
+    font-weight: bold
+
 @media (max-width: 1920px)
 
 //'2xl': '1536px',
@@ -43,6 +48,19 @@ section.center.dark-glass
 @media (max-width: 1024px)
 //'md': '768px', phones
 @media (max-width: 767px)
+    .table
+        border: 1px solid rgba(255,255,255,.25)
+        >*
+            border-top: 1px solid rgba(255,255,255,.25)
+            padding: .5rem
+    .table-title
+        background: white
+        color: black
+        width: fit-content
+        padding: .5rem 2rem
+        font-weight: bold
+                
+
 //'sm': '640px', small phones
 @media (max-width: 640px)
 </style>
